14304a4698717ffd8c79eae4a1029a8b10a56279
kaanzapkinus <kaanyazicioglu35@hotmail.com>
2025-03-11 23:10:38 +0100

    file management updates

--- lecture1/task of lecture 1/main.py
+++ lecture1/task of lecture 1/main.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 if __name__ == '__main__':
     file = 'LifeExpectancy.csv' 
-df = pd.read_csv(file, header=0)
-print(df.head(5))
+    df = pd.read_csv(file, header=0)
+
+    for col in df.columns:
+        print(col)
+
+    print(df.head(5))
